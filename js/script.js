4f35abb052962b3708eb7ec9a4e83c1feeb42dfc
Destonin <destonin@gmail.com>
2019-03-10 14:01:48 +0200

    Настроил увеличение изображение

--- js/script.js
+++ js/script.js
@@ -57,4 +57,20 @@ $(document).ready(function(){
   		$(this).toggleClass('size_btn_active');
   	});
 
+  	$(".product_info_image_click").click(function(){	// Событие клика на маленькое изображение
+	  	var img = $(this);	// Получаем изображение, на которое кликнули
+		var src = img.attr('src'); // Достаем из этого изображения путь до картинки
+		$("body").append("<div class='popup'>"+ //Добавляем в тело документа разметку всплывающего окна
+						 "<div class='popup_bg'></div>"+ // Блок, который будет служить фоном затемненным
+						 "<img src='"+src+"' class='popup_img' />"+ // Само увеличенное фото
+						 "</div>"); 
+		$(".popup").fadeIn(400); // Медленно выводим изображение
+		$(".popup_bg").click(function(){	// Событие клика на затемненный фон	   
+			$(".popup").fadeOut(400);	// Медленно убираем всплывающее окно
+			setTimeout(function() {	// Выставляем таймер
+			  $(".popup").remove(); // Удаляем разметку всплывающего окна
+			}, 400);
+		});
+	});
+
 });
